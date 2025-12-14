a9823fb3e380d7f5ac530034d1661cee5940d386
dakuraja <mk298637@gmail.com>
2025-12-14 12:12:23 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-set -o errexit
+set -e
+
+echo "Updating system..."
+apt-get update -y
+
+echo "Installing base dependencies..."
+apt-get install -y wget fontconfig xfonts-75dpi xfonts-base
+
+echo "Downloading wkhtmltopdf..."
+wget -q https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6/wkhtmltox_0.12.6-1.focal_amd64.deb
 
 echo "Installing wkhtmltopdf..."
+dpkg -i wkhtmltox_0.12.6-1.focal_amd64.deb || apt-get -f install -y
 
-apt-get update
-apt-get install -y wget xfonts-75dpi xfonts-base
-
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.6/wkhtmltox_0.12.6-1.focal_amd64.deb
-
-apt install -y ./wkhtmltox_0.12.6-1.focal_amd64.deb
-
+echo "Verifying installation..."
 wkhtmltopdf --version
